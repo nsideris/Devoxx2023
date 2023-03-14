e37f3487cedbe6b15f6e62d8c6684377f9b5086e
Nikos Sideris <n.sideris@thesignalgroup.com>
2023-03-14 12:00:02 +0200

    remove unused config build

--- build/Build.cs
+++ build/Build.cs
@@ -16,9 +16,7 @@ class Build : NukeBuild
 {
     public static int Main() => Execute<Build>(x => x.BlueGreenDeploy);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-
+ 
     [Solution] readonly Solution Solution;
 
     string Version = "1.0.6";
